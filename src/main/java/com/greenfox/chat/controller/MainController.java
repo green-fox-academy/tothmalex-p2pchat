ee62adec89e8023165da48f87b2c7ef24f9fbf69
Alex Toth <drive_alex@hotmail.com>
2017-11-10 09:52:07 +0100

    init com

--- src/main/java/com/greenfox/chat/controller/MainController.java
+++ src/main/java/com/greenfox/chat/controller/MainController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("")
 public class MainController {
 
     @GetMapping({"", "/"})
     public String openingMain(  ) {
-        return "indexAlex";
+        return "index";
     }
 }
