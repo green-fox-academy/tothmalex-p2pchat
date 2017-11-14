4a61ab937f355a7520fa70aa052cf728e8e98518
Alex Toth <drive_alex@hotmail.com>
2017-11-14 13:59:31 +0100

    main page structure

--- src/main/java/com/greenfox/chat/controller/MainController.java
+++ src/main/java/com/greenfox/chat/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.greenfox.chat.controller;
 
 import com.greenfox.chat.model.ChatUser;
+import com.greenfox.chat.model.Message;
+import com.greenfox.chat.repository.ChatRepo;
 import com.greenfox.chat.repository.UserRepo;
 import com.greenfox.chat.serrvice.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +18,12 @@ public class MainController {
 
     @Autowired
     UserRepo userRepo;
+    ChatRepo chatRepo;
 
     @Autowired
     LogService logService;
+
+
 
     long id = 1;
 
@@ -36,9 +41,12 @@ public class MainController {
         return "redirect:/index";
     }
 
-    @GetMapping("/index")
-    public String index( HttpServletRequest request ) {
+    @RequestMapping("/")
+    public String getHomepage(Model model, HttpServletRequest request) {
         logService.checkEnvironment(request);
+        model.addAttribute("user", userRepo.findAll());
+        model.addAttribute("newMessage", new Message());
+        model.addAttribute("messages", chatRepo.findAll());
         return "index";
     }
 
--- src/main/java/com/greenfox/chat/repository/UserRepo.java
+++ src/main/java/com/greenfox/chat/repository/UserRepo.java
@@ -1,6 +1,10 @@
 package com.greenfox.chat.repository;
 
 import com.greenfox.chat.model.ChatUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepo extends CrudRepository<ChatUser, Long> {}
+public interface UserRepo extends CrudRepository<ChatUser, Long> {
+
+    String findOneUser(Long id);
+}
